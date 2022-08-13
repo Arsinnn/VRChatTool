65738fafa9e22b2b38feebcc3acc5c6d07d6c18a
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-08-13 15:11:18 -0400

    joe

--- main.go
+++ main.go
@@ -1,42 +1,31 @@
 package main
 
 import (
-	"bufio"
-	"errors"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
-func main() {
-	Start()
-}
-
 var authcookie string
 
+func main() {
+	ReadFile, _ := os.ReadFile("Account.txt")
+	if string(ReadFile) == "" {
+		fmt.Println("Couldn't find account\nEnter your account info in user:password format")
+		var UserPass string
+		fmt.Scanln(&UserPass)
+		os.WriteFile("Account.txt", []byte(UserPass), 0)
+	}
+	GetAuthCookie()
+}
+
 func Start() {
-	Reader := bufio.NewReader(os.Stdin)
-	if _, err := os.Stat("Authcookie.txt"); errors.Is(err, os.ErrNotExist) {
-		os.Create("Authcookie.txt")
-	}
-	if _, err := os.Stat("Authcookies.txt"); errors.Is(err, os.ErrNotExist) {
-		os.Create("Authcookies.txt")
-	}
-	if _, err := os.Stat("Avatars.txt"); errors.Is(err, os.ErrNotExist) {
-		os.Create("Avatars.txt")
-	}
-	joe, _ := os.ReadFile("Authcookie.txt")
-	if !(strings.Contains(string(joe), "authcookie_")) {
-		fmt.Println("authcookie not found enter your authcookie")
-		Input, _ := Reader.ReadString('\n')
-		os.WriteFile("Authcookie.txt", []byte(Input), 0)
-	}
-	joe2, _ := os.ReadFile("Authcookie.txt")
-	authcookie = string(joe2)
 	fmt.Println("-----Pick a option-----")
-	fmt.Println("1: Request spam UserID")
+	fmt.Println("1: Request spam UserID\n2: UserID lookup")
 	var Input string
 	fmt.Scanln(&Input)
 	switch Input {
@@ -48,6 +37,11 @@ func Start() {
 			go RequestSpam(UserID)
 		}
 		RequestSpam(UserID)
+	case "2":
+		fmt.Println("Enter UserID to lookup")
+		var UserID string
+		fmt.Scanln(&UserID)
+		UserSearch(UserID)
 	default:
 		fmt.Println("Pick a valid option")
 		time.Sleep(3 * time.Second)
@@ -56,16 +50,73 @@ func Start() {
 }
 
 func RequestSpam(UserID string) {
-	client := http.Client{}
+	client := &http.Client{}
 	for i := 0; i < 25; i++ {
 		request, _ := http.NewRequest("POST", "https://api.vrchat.cloud/api/1/requestInvite/"+UserID, strings.NewReader("{\"platform\":\"standalonewindows\"}"))
-		request.Header.Add("Cookie", "apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26; auth="+authcookie)
-		request.Header.Add("X-Client-Version", "2022.2.2-1213--Release")
-		request.Header.Add("X-Platform", "standalonewindows")
-		request.Header.Add("Content-Type", "application/json")
-		request.Header.Add("User-Agent", "Transmtn-Pipeline")
-		request.Header.Add("Host", "api.vrchat.cloud")
+		request.Header = http.Header{
+			"Cookie":           {"apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26; auth=" + authcookie},
+			"X-Client-Version": {"2022.2.2-1213--Release"},
+			"X-Platform":       {"standalonewindows"},
+			"Content-Type":     {"application/json"},
+			"User-Agent":       {"Transmtn-Pipeline"},
+			"Host":             {"api.vrchat.cloud"},
+		}
 		response, _ := client.Do(request)
 		fmt.Println(response.Status)
 	}
+}
+
+func UserSearch(UserID string) {
+	client := &http.Client{}
+	request, _ := http.NewRequest("GET", "https://api.vrchat.cloud/api/1/users/"+UserID+"?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26&organization=vrchat", nil)
+	request.Header = http.Header{
+		"X-Requested-With": {"XMLHttpRequest"},
+		"X-Macaddress":     {"3ceb8cc8df874eeba6ff679158315ec54f1470eb"},
+		"X-Client-Version": {"2022.2.2p2c-1221--Release"},
+		"X-Platform":       {"standalonewindows"},
+		"X-Unity-Version":  {"2019.4.31f1"},
+		"Content-Type":     {"application/x-www-form-urlencoded"},
+		"Origin":           {"vrchat.com"},
+		"Host":             {"api.vrchat.cloud"},
+		"Connection":       {"Keep-Alive, TE"},
+		"TE":               {"identity"},
+		"User-Agent":       {"VRC.Core.BestHTTP"},
+		"Cookie":           {"auth=authcookie_be4ed7e6-f192-4ea8-87ed-cc8da07d0124; apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26; twoFactorAuth="},
+		"accept":           {"*/*"},
+		"Accept-Encoding":  {"identity"},
+	}
+	response, _ := client.Do(request)
+	body, _ := io.ReadAll(response.Body)
+	Formated := strings.Split(string(body), ",")
+	for i := 0; i < len(Formated); i++ {
+		fmt.Println(Formated[i])
+	}
+}
+
+func GetAuthCookie() {
+	ReadFile, _ := os.ReadFile("Account.txt")
+	client := &http.Client{}
+	request, _ := http.NewRequest("GET", "https://api.vrchat.cloud/api/1/auth/user?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26&organization=vrchat", nil)
+	request.Header = http.Header{
+		"X-Requested-With": {"XMLHttpRequest"},
+		"X-MacAddress":     {"3ceb8cc8df874eeba6ff679158315ec54f1470eb"},
+		"X-Client-Version": {"2022.2.2p2c-1221--Release"},
+		"X-Platform":       {"standalonewindows"},
+		"X-Unity-Version":  {"2019.4.31f1"},
+		"Content-Type":     {"application/x-www-form-urlencoded"},
+		"Origin":           {"vrchat.com"},
+		"Host":             {"api.vrchat.cloud"},
+		"Connection":       {"Keep-Alive, TE"},
+		"TE":               {"identity"},
+		"User-Agent":       {"VRC.Core.BestHTTP"},
+		"Authorization":    {"Basic " + base64.StdEncoding.EncodeToString(ReadFile)},
+		"Cookie":           {"apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26; twoFactorAuth="},
+		"Accept-Encoding":  {"identity"},
+	}
+	response, _ := client.Do(request)
+	body, _ := io.ReadAll(response.Body)
+	Format2 := strings.Split(string(body), "\"")
+	fmt.Println("Logged in as: " + Format2[11])
+	authcookie = response.Cookies()[0].Value
+	Start()
 }
